1b80820bc673b1a464c1529be1da973f7f7209f3
mehmet <mhmt.kocaturk@gmail.com>
2020-07-14 17:39:28 +0300

    ddd

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("git1")
-        print("git2")
+        print("git2") 
     }
 
 
